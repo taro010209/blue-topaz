67b692664304fc7c69030ba3558d17f84a33806a
taro <tofj95@gmail.com>
2023-01-01 21:29:26 +0900

    Netlify試行

--- src/components/main/about/About.vue
+++ src/components/main/about/About.vue
@@ -11,7 +11,7 @@
       <h2 id="about" class="about__heading">
         <span>一緒に</span>
         <span>おどりませんか？</span>
-        <span>的な</span>
+        <!-- <span>的な</span> -->
       </h2>
       <p class="about__lead">
         Blue Topazは2018年に創部したチアダンス部です。<br /><br />
